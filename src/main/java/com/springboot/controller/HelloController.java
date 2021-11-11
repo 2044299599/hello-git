2a3a0ba1870f87c517a127151943b9e734379d3a
thb <2044299599@qq.com>
2021-11-12 03:37:46 +0800

    zz

--- src/main/java/com/springboot/controller/HelloController.java
+++ src/main/java/com/springboot/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
-        System.out.println("哈哈哈，嘻嘻嘻");
+        System.out.println("哈哈哈，嘻嘻嘻z");
         return "hello";
     }
 }
